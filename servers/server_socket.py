e52e5d6208bb3ed3562f31c32a01885af37d90c9
Zhenya <zhenya.subbota.19@mail.ru>
2020-03-21 15:37:50 +0300

    threading server

--- servers/server_socket.py
+++ servers/server_socket.py
@@ -1,9 +1,37 @@
 import socket
+from threading import Thread
+
+BUF_SIZE = 4096
+
+
+class ClientThread(Thread):
+
+    def __init__(self, client, operation_obj):
+        Thread.__init__(self)
+        self.client = client
+        self.operation_obj = operation_obj
+
+    def run(self):
+
+        while True:
+            try:
+                self.client.settimeout(5)
+                operation_message = self.client.recv(BUF_SIZE).decode('utf-8')
+                if not operation_message:
+                    break
+                self.operation_obj.dict_with_operation[operation_message[:2]](self.client, operation_message)
+            except KeyError:
+                continue
+            except ConnectionResetError:
+                continue
+            except socket.timeout:
+                continue
 
 
 class ServerSocket:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     def socket_connect(self, tuple_address_port=('127.0.0.1', 8001), number_listen=5):
         self.sock.bind(tuple_address_port)
